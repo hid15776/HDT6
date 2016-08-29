6bbd286acb378b75fe3d3922ba26407f0d39e0b2
hid15776 <hid15776@uvg.edu.gt>
2016-08-29 14:45:31 -0600

    Update main.java

--- main.java
+++ main.java
@@ -1,3 +1,10 @@
+/**
+ * @author Gladys de la Roca, Jackeline Hidalgo
+ * Clase Main
+ * Main del programa
+ * @version 29/08/2016
+ */
+ 
 public class Main {
 
     public static void main(String[] args) {
