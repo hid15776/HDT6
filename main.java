fb21bae6c36694c11a4a443cb8a0d711a21335ba
Gladys20 <dela15755@uvg.edu.gt>
2016-08-29 11:32:31 -0600

    Create main.java

---
+++ main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Conjunto con = new Conjunto(); //Inicia los conjuntos
+        con.ingresarDesarrollador(); //ingresa a los desarrolladores
+        con.resultados(); //imprime los resultados
+    }
+}
